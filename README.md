53e1a03fec4272d7584adc090ea77b8d994a30e7
oHixDio <hry66H@outlook.jp>
2024-04-28 20:48:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Universe
+
+Developed with Unreal Engine 5
---
+++ Source/Universe/UniverseGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UniverseGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUniverseGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUniverseGameMode();
+};
+
+
+
